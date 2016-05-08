81572f924cad1098009d124efa18b16621988788
ReasonDuan <duan_huanlong@163.com>
2016-05-08 22:29:33 +0800

    web 接口

--- src/main/java/com/ams/controller/admin/ServiceController.java
+++ src/main/java/com/ams/controller/admin/ServiceController.java
@@ -1,14 +1,15 @@
 package com.ams.controller.admin;
 
-import com.ams.entities.admin.ProductInfo;
-import com.ams.entities.admin.ServiceRequest;
-import com.ams.entities.admin.ServiceResponse;
+import com.alibaba.fastjson.JSONObject;
+import com.ams.entities.admin.*;
 import com.ams.service.admin.ProductService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,34 +28,41 @@ public class ServiceController extends BaseController {
     @Autowired
     ProductService productService;
 
-    @RequestMapping(value = "add")
-    @ResponseBody
-    public ServiceResponse add(HttpServletRequest request, HttpServletResponse response, ModelMap model, ProductInfo productInfo) {
+    @RequestMapping(value = "rest/save")
+    public @ResponseBody ServiceResponse add(HttpServletRequest request, HttpServletResponse response, ModelMap model,@RequestBody ProductInfoTemp productInfo) {
         logger.debug(productInfo);
-
-        productInfo.setStatus("2");
-
-
         ServiceResponse res = new ServiceResponse();
+        try {
+            productInfo.setStatus("2");
+            //productService.saveProduct(productInfo);
 
-        //productService.saveProduct(productInfo);
+        }catch (Exception e) {
+            logger.error("保存失败。", e);
+            res.setCode(500);
+            res.setMessage(e.getMessage());
+        }
+
         return res;
     }
 
 
     @RequestMapping(value = "rest/list")
-    @ResponseBody
-    public ServiceResponse list(HttpServletRequest request, HttpServletResponse response, ModelMap model, ServiceRequest serviceRequest) {
+    public  @ResponseBody ServiceResponse list(HttpServletRequest request, HttpServletResponse response, ModelMap model, ServiceRequest serviceRequest) {
 
         logger.info(serviceRequest);
         ServiceResponse res = new ServiceResponse();
-        List<ProductInfo> list = productService.serviceQueryList(serviceRequest);
-        res.setData(list);
+        try {
+            List<ProductInfo> list = productService.serviceQueryList(serviceRequest);
+            logger.info(list.get(0));
+            res.setData(list);
+        }catch (Exception e){
+            logger.error("获取成果、专利失败。",e);
+            res.setCode(500);
+            res.setMessage(e.getMessage());
+        }
 
-        //productService.saveProduct(productInfo);
         return res;
     }
-
 
 
 }
